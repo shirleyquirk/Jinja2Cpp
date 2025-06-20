75909dcad5dd7686b36c43d7fcd23f5ecae1ad0b
Benjamin Shirley-Quirk <shirleyquirk@gmail.com>
2025-06-20 23:42:27 +0100

    remove nonstd deps from cmake

--- thirdparty/internal_deps.cmake
+++ thirdparty/internal_deps.cmake
@@ -1,41 +1,5 @@
 include(FetchContent)
-
-FetchContent_Declare(
-    expected-lite
-    URL https://github.com/martinmoene/expected-lite/archive/refs/tags/v0.8.0.tar.gz
-    URL_HASH SHA256=27649f30bd9d4fe7b193ab3eb6f78c64d0f585c24c085f340b4722b3d0b5e701
-)
-FetchContent_MakeAvailable(expected-lite)
-
-FetchContent_Declare(
-    variant-lite
-    URL https://github.com/martinmoene/variant-lite/archive/5015e841cf143487f2d7e2f619b618d455658fab.tar.gz
-    URL_HASH SHA256=d343cfa347517a2ee318864f3e2a08af8e17e699de701c69c1cdbdab79d9331f
-)
-FetchContent_MakeAvailable(variant-lite)
-
-FetchContent_Declare(
-    optional-lite
-    URL https://github.com/martinmoene/optional-lite/archive/refs/tags/v3.6.0.tar.gz
-    URL_HASH SHA256=2be17fcfc764809612282c3e728cabc42afe703b9dc333cc87c48d882fcfc2c2
-)
-FetchContent_MakeAvailable(optional-lite)
-
-FetchContent_Declare(
-    string-view-lite
-    URL https://github.com/martinmoene/string-view-lite/archive/refs/tags/v1.8.0.tar.gz
-    URL_HASH SHA256=9b38c32621eb1a81a7fa59427144309225c414a7bae522ab3a2d9ae239dd35be
-)
-FetchContent_MakeAvailable(string-view-lite)
-
-set (FMT_INSTALL ON CACHE BOOL "" FORCE)
-FetchContent_Declare(
-    fmt
-    URL https://github.com/fmtlib/fmt/archive/refs/tags/10.2.1.tar.gz
-    URL_HASH SHA256=1250e4cc58bf06ee631567523f48848dc4596133e163f02615c97f78bab6c811
-)
-FetchContent_MakeAvailable(fmt)
-
+find_package(FMT REQUIRED)
 set (RAPIDJSON_BUILD_DOC OFF CACHE BOOL "" FORCE)
 set (RAPIDJSON_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 set (RAPIDJSON_BUILD_TESTS OFF CACHE BOOL "" FORCE)
