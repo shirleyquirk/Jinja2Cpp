f8a005d6d02006f3fb104eec9d222d3853291491
Benjamin Shirley-Quirk <shirleyquirk@gmail.com>
2025-06-21 00:19:22 +0100

    i do NOT understand why we have to return as well as put into a store

--- src/serialize_filters.cpp
+++ src/serialize_filters.cpp
@@ -233,7 +233,8 @@ struct FormatArgumentConverter : visitors::BaseVisitor<FormatArgument>
         {
             m_store.push_back(fmt::arg(m_name.c_str(), t));
         }
-        return fmt::detail::make_arg<FormatContext>(t);
+        //return fmt::detail::make_arg<FormatContext>(t);
+        return fmt::basic_format_args<FormatContext>(m_store).get(m_store.size()-1);
     }
 
     const RenderContext* m_context;
